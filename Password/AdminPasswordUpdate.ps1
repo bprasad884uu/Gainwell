edcdb560781bb2d3a8405d84333c2609e1ef7da3
Coder_Devil <bishnuprasad884uu@gmail.com>
2025-06-21 15:17:49 +0530

    Admin account rename and update accordingly

--- Password/AdminPasswordUpdate.ps1
+++ Password/AdminPasswordUpdate.ps1
@@ -1,3 +1,34 @@
+# Detect OS type: 1 = Workstation, 2 = Domain Controller, 3 = Server
+$OSType = (Get-CimInstance Win32_OperatingSystem).ProductType
+
+# Only proceed if OS is client (ProductType = 1)
+if ($OSType -ne 1) {
+    Write-Host "Non-client OS detected. Rename skipped."
+    exit
+}
+
+# Find built-in Administrator account (RID 500)
+$AdminUser = Get-LocalUser | Where-Object { $_.SID -like "S-1-5-21-*-500" } | Select-Object -ExpandProperty Name
+
+# Desired admin name for client OS
+$WinOSAdmin = "gcpladmusr"
+#$ServerOSAdmin = "gcpladmwusr"
+
+if ($AdminUser) {
+    try {
+        if ($AdminUser -eq $WinOSAdmin) {
+            #Write-Host "Administrator account already named '$WinOSAdmin'. No changes made."
+        } else {
+            Rename-LocalUser -Name $AdminUser -NewName "$WinOSAdmin"
+            Write-Host "Administrator account renamed!"
+        }
+    } catch {
+        Write-Host "Error: Unable to rename Administrator account. $_"
+    }
+} else {
+    Write-Host "Administrator account not found!"
+}
+
 # AES Decryption Script
 
 # Input the encrypted text
@@ -32,10 +63,10 @@ try {
 }
 
 # Check if the Administrator account is already active
-$adminUser = Get-LocalUser -Name "Administrator"
+$adminUser = Get-LocalUser -Name "gcpladmusr"
 if ($adminUser.Enabled -eq $false) {
     # Activate the Administrator account if it's not already active
-    Enable-LocalUser -Name "Administrator"
+    Enable-LocalUser -Name "gcpladmusr"
     Write-Host "Administrator Account Activated."
 } else {
     #Write-Host "Administrator Account is already active."
@@ -44,12 +75,12 @@ if ($adminUser.Enabled -eq $false) {
 # Check if the full name is correct
 if ($adminUser.FullName -ne $desiredFullName) {
     # Update the full name if it doesn't match
-    Set-LocalUser -Name "Administrator" -FullName $desiredFullName
+    Set-LocalUser -Name "gcpladmusr" -FullName $desiredFullName
     Write-Host "Full name has been updated for the Administrator account."
 } else {
     #Write-Host "Full name is already set to '$desiredFullName'."
 }
 
 # Set the new password for the Administrator account
-Set-LocalUser -Name "Administrator" -Password (ConvertTo-SecureString -String $Password -AsPlainText -Force)
+Set-LocalUser -Name "gcpladmusr" -Password (ConvertTo-SecureString -String $Password -AsPlainText -Force)
 Write-Host "Password has been updated for Administrator account."
