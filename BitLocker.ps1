3f34c9af4de9453e4806f284e93397405e9f4704
Coder_Devil <bishnuprasad884uu@gmail.com>
2025-07-19 16:47:03 +0530

    Add Real time progress

--- BitLocker.ps1
+++ BitLocker.ps1
@@ -3,9 +3,22 @@ $drives = Get-BitLockerVolume
 
 foreach ($drive in $drives) {
     if ($drive.VolumeStatus -eq 'FullyEncrypted' -or $drive.VolumeStatus -eq 'EncryptionInProgress') {
-        Write-Output "Disabling BitLocker on drive $($drive.MountPoint)"
+        Write-Output "`nDisabling BitLocker on drive $($drive.MountPoint)..."
         Disable-BitLocker -MountPoint $drive.MountPoint
+
+        # Monitor the decryption progress in real-time
+        do {
+            Start-Sleep -Seconds 5
+            $updatedDrive = Get-BitLockerVolume -MountPoint $drive.MountPoint
+            $percentage = $updatedDrive.EncryptionPercentage
+
+            if ($null -ne $percentage) {
+                Write-Host "`rProgress: $percentage% " -NoNewline
+            }
+        } while ($updatedDrive.VolumeStatus -ne 'FullyDecrypted')
+
+        Write-Host "`rProgress: 100% - Drive $($drive.MountPoint) fully decrypted.`n"
     } else {
-        Write-Output "BitLocker is not enabled on drive $($drive.MountPoint)"
+        Write-Host "`nBitLocker is not enabled on drive $($drive.MountPoint)`n"
     }
 }
