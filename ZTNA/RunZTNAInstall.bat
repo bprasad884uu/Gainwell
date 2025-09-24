b7ef96634ea3591d5714a5a3b546d1af85478e87
Coder_Devil <bishnuprasad884uu@gmail.com>
2025-09-24 16:30:26 +0530

    Corrected script name

--- ZTNA/RunZTNAInstall.bat
+++ ZTNA/RunZTNAInstall.bat
@@ -3,7 +3,7 @@ setlocal
 cd /d "%~dp0"
 
 echo Running PowerShell script...
-powershell.exe -ExecutionPolicy Bypass -NoProfile -File ".\Uninstall_Forti_Install_ZTNA.ps1"
+powershell.exe -ExecutionPolicy Bypass -NoProfile -File ".\Install_ZTNA.ps1"
 
 endlocal
 exit /b 0
