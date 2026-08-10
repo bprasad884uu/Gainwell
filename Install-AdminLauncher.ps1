44790754104226dcde82ede69a5adaa4a1a93cc4
Coder_Devil <bishnuprasad884uu@gmail.com>
2026-08-10 10:48:03 +0530

    fix: move AdminLauncher app metadata to registration block

--- Install-AdminLauncher.ps1
+++ Install-AdminLauncher.ps1
@@ -41,9 +41,6 @@ $AllowList        = Join-Path $BaseDir "allowlist.json"
 $CertPath         = Join-Path $CertDir "AcceleronCertificate.cer"
 $LogFile          = Join-Path $BaseDir "install.log"
 
-$AppName          = "Admin Launcher"
-$AppVersion       = if (Test-Path $ExeWrapperPath) { (Get-Item $ExeWrapperPath).VersionInfo.FileVersion } else { $null }
-
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall\AdminLauncher"
 
 # Calculate folder size in KB
@@ -279,6 +276,9 @@ public static extern IntPtr SendMessageTimeout(IntPtr hWnd, uint Msg, UIntPtr wP
 Add-ToSystemPath -PathToAdd $BaseDir
 
 # Register application
+
+$AppName          = "Admin Launcher"
+$AppVersion       = if (Test-Path $ExeWrapperPath) { (Get-Item $ExeWrapperPath).VersionInfo.FileVersion } else { $null }
 
 New-Item -Path $RegPath -Force | Out-Null
 
